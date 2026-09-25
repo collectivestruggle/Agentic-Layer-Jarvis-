4f1d89a1f2ae748dd2d71c73086f3bc3c57b4a26
Collective Struggle Foundry <geoffrey@collectivestruggle.info>
2026-09-24 17:23:02 -0700

    agent2: add isolated wrapper package

---
+++ agent2/__init__.py
@@ -0,0 +1,5 @@
+"""Agent 2's isolated OpenJarvis wrapper."""
+
+from .wrapper import AGENT2_ID, build_agent2_mcp, build_agent2_system
+
+__all__ = ["AGENT2_ID", "build_agent2_mcp", "build_agent2_system"]
